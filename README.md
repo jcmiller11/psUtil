57feb8434ee0272871acc09e32f063387bf5a025
John C Miller <jcmiller@berkshirephotonics.com>
2019-09-09 13:38:15 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # psUtil
 Text utilities for puzzlescript sprites and levels
+
+A few text transformations I found myself often wanting while working with [PuzzleScript](https://github.com/increpare/PuzzleScript) by [increpare](https://github.com/increpare)
---
+++ psutil.js
@@ -0,0 +1,41 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+function flipHorizontal(str) {
+	lines = str.split("\n");
+	lines = lines.map(function(line) { 
+	  return reverseString(line);
+	});
+	return lines.join("\n");
+}
+function flipVertical(str) {
+	return str.split("\n").reverse().join("\n");
+}
+//TODO: rewrite function below
+//I'm sure there is a cleaner way and it doesn't handle trailing whitespace as well as it could
+function rotate(str) {
+	lines = str.split("\n");
+	spunArray = new Array();
+	longest = 0;
+	lines.forEach(function(line) {
+		if(line.length>longest) {
+			longest = line.length;
+		}
+	})
+	lines = lines.map(function(line) {
+		return line.padEnd(longest);
+	})
+	for(let y = 0;y<longest;y++) {
+		spunArray[y] = new Array();
+		for(let x = 0;x<lines.length;x++) {
+			spunArray[y][x]=lines[x].charAt(y);
+		}
+	}
+	lines = spunArray.map(function(line) {
+		return line.join("");
+	})
+	return flipHorizontal(lines.join("\n"));
+}
+function changeInput(theFunction) {
+	$('#input').val(theFunction($.trim($('#input').val())))
+}
